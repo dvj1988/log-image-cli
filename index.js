9d8eba1fb0d907fa611a74cd45b486f38598fb69
Diljit VJ <diljitvj@gmail.com>
2019-05-11 20:08:14 +0530

    Update cli tool to accept width percentage

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 const logImg = require("log-image");
 const path = require("path");
 const filePath = process.argv[2];
+const widthPercentage = process.argv[3];
 
-console.log(logImg(path.resolve(filePath)));
+console.log(logImg(path.resolve(filePath), widthPercentage));

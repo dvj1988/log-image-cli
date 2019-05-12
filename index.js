a8c718f20a679a4ed961b3785dd02a05c01c16ae
Diljit VJ <diljitvj@gmail.com>
2019-05-12 21:10:19 +0530

    1. Add the new argument with width 2. Update the log-image dependency version

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const logImg = require("log-image");
 const path = require("path");
 const filePath = process.argv[2];
-const widthPercentage = process.argv[3];
+const widthParamIndex = process.argv.findIndex(arg => arg === "--width");
+const widthPercentage =
+  widthParamIndex !== -1 ? process.argv[widthParamIndex + 1] : 100;
 
 console.log(logImg(path.resolve(filePath), widthPercentage));
